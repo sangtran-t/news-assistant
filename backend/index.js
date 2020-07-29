fc2e9a0cee585a3490982b1de1632cd5bc57536d
sangtran <sangdonga25@gmail.com>
2020-07-29 15:27:33 +0700

    deploy heroku

--- backend/index.js
+++ backend/index.js
@@ -9,40 +9,24 @@ require("dotenv").config();
 
 // Connect to mongodb
 mongoose
-  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true })
     .catch((error) => handleError(error));
 
 mongoose.connection.once("open", () => console.log("Connect Database Successfully!"));
 
-// app.all('/', function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next()
-// });
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Backend MRC is running!"));
 
 app.get("/articles", (req, res) => {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
     database.GetAllArticles(req, res);
 });
 
 app.get("/audio", (req, res) => {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
     database.GetAudio(req, res);
 });
 
 app.get("/contents", (req, res) => {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
     database.GetContents(req, res);
 });
 
